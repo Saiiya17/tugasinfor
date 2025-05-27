1fe7b39512dd06903b3baa2daac0f9e83a1df54d
Saiiya17 <s4iiyaaprill4@gmail.com>
2025-05-27 11:28:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("🎈 saiiyalahirbulanaprilla")
 st.write(
-    "jajanserubarengsaiiya"
+    "seruseruan"
 )
 st.image("7d9d78edbabfa5d7e65d1e36b85c230d.jpg", width=200)
